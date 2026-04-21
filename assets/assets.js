f64caec71ae2b6912d9fe81da91e6c1ed8b4661e
junartisan <your-github-email@example.com>
2026-04-21 20:40:03 +0800

    update

--- assets/assets.js
+++ assets/assets.js
@@ -104,7 +104,7 @@ export const workData = [
 ];
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
+    { icon: assets.web_icon, title: 'Software Development', description: 'Software development is the process of planning, designing, programming...', link: '' },
     { icon: assets.mobile_icon, title: 'Web & Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
     { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
     { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
